540bf4ef49b7cd9c3dd170f7b04decdba56ae562
Evan Lin <evanslin@gmail.com>
2017-04-11 01:39:21 +0800

    foo bar for debugging

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -25,6 +26,15 @@ func serveHttpAPI(port string, existC chan bool) {
 		jobAPI.POST("/", CreateTFImage)
 	}
 
+	fooAPI := router.Group("/api/v1/foo")
+	{
+		fooAPI.POST("/", CreatFoo)
+		fooAPI.GET("/", TestFoo)
+		// 	rsAPI.GET("/:id", FetchSingleJob)
+		// 	rsAPI.PUT("/:id", UpdateJob)
+		// 	rsAPI.DELETE("/:id", DeleteJob)
+	}
+
 	// rsAPI := router.Group("/api/v1/resources")
 	// {
 	// 	rsAPI.POST("/", CreateJob)
@@ -45,4 +55,22 @@ func CreateTFImage(c *gin.Context) {
 	// defer db.Close()
 	// db.Save(&todo)
 	// c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "resourceId": todo.ID})
+}
+
+//CreatFoo :
+func CreatFoo(c *gin.Context) {
+	log.Println("CreatFoo:", c)
+	// completed, _ := strconv.Atoi(c.PostForm("completed"))
+	// todo := Todo{Title: c.PostForm("title"), Completed: completed}
+	// db, _ := Database()
+	// defer db.Close()
+	// db.Save(&todo)
+	// c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "resourceId": todo.ID})
+}
+
+//TestFoo :
+func TestFoo(c *gin.Context) {
+	ret := "bar"
+	log.Println("TestFoo:", c)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": ret})
 }
